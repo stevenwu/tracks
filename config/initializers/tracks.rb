4215bfe133d85ac4967d083fceca167ec931c580
Katrina Owen <katrina.owen@gmail.com>
2013-04-27 10:56:34 -0600

    Delete version info from footer
    In the refactoring workshop we use a lockdown test that saves the (html)
    output of an entire request. We use timecop to work around any changing
    times and dates, but the version information in the footer contains a
    timestamp that isn't affected by this because that is loaded before
    timecop is triggered.
    
    To avoid ugly workarounds, I'm simply removing it in this fork.

--- config/initializers/tracks.rb
+++ config/initializers/tracks.rb
@@ -4,7 +4,7 @@ NOTIFY_BAR = "" unless defined?(NOTIFY_BAR)
 tracks_version='2.3devel'
 # comment out next two lines if you do not want (or can not) the date of the
 # last git commit in the footer
-info=`git log --pretty=format:"%ai" -1`
-tracks_version=tracks_version + ' ('+info+')'
+# info=`git log --pretty=format:"%ai" -1`
+# tracks_version=tracks_version + ' ('+info+')'
 
 TRACKS_VERSION=tracks_version
